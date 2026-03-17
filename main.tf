3471f0707b2aa20b15c3a12bece07b01383ee454
Debs07 <59568465+Debs07@users.noreply.github.com>
2026-03-17 16:23:20 +0000

    Modify Terraform provider and backend settings
    Updated the subscription ID and storage account name in the Terraform configuration.

--- main.tf
+++ main.tf
@@ -9,13 +9,13 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = "<your subscription id here>"
+  subscription_id = "3eb2d17a-331c-455f-afd5-3fcb4b051489"
 }
 
 terraform {
   backend "azurerm" {
     resource_group_name  = "RG1"
-    storage_account_name = "<your storage account>"
+    storage_account_name = "jenkinsstatedeboramagi"
     container_name       = "terraform-state"
     key                  = "terraform.tfstate"
   }
